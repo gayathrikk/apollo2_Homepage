e25026a5f472633d7c5e2c3793f7fee88c4cbe70
gayathrikk <gayathri@htic.iitm.ac.in>
2024-01-12 11:25:37 +0530

    update commit

---
+++ src/main/java/com/apollo2/apollo2_Homepage/App.java
@@ -0,0 +1,13 @@
+package com.apollo2.apollo2_Homepage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/apollo2/apollo2_Homepage/AppTest.java
@@ -0,0 +1,20 @@
+package com.apollo2.apollo2_Homepage;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
