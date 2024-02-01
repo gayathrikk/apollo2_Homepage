8ae1cf84791e6fabc74aa917a8e8f1a2d2d469ad
gayathrikk <gayathri@htic.iitm.ac.in>
2024-02-01 10:20:08 +0530

    first commit

--- src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
+++ src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
@@ -175,7 +175,7 @@ private RemoteWebDriver driver;
     public void homepagehighlights() throws InterruptedException
     {
     	 driver.findElement(By.xpath("(//a[text()='HIGHLIGHTS'])[1]")).click();
-    	 driver.findElement(By.xpath("//button[text()='Neuroanatomy Histology']")).click();
+    	 driver.findElement(By.xpath("//button[@class='tablinks active']")).click();
 		  scroll();
 		 driver.findElement(By.xpath("//button[text()='Computational']")).click();
 		  scroll();
