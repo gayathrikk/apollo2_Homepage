b5bebcee49ec3266934980036be98632cc12530a
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-06-20 12:09:00 +0530

    sanitytesting.java

--- src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
+++ src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
@@ -32,7 +32,7 @@ private RemoteWebDriver driver;
 	public void setup() throws MalformedURLException
 	{
 		  DesiredCapabilities dc = DesiredCapabilities.chrome();
-	        URL url = new URL("http://172.12.20.99:4444/wd/hub");
+	        URL url = new URL("http://172.20.23.92:4444/wd/hub");
 	        driver = new RemoteWebDriver(url, dc);
 	       
 	}
