e3526f50c40aa6798cd46d3a83835ea45dbc5f37
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-12-09 12:23:44 +0530

    Update sanitytesting.java

--- src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
+++ src/test/java/com/apollo2/apollo2_Homepage/sanitytesting.java
@@ -59,7 +59,7 @@ private RemoteWebDriver driver;
 	   Assert.assertEquals(Heading1, heading1, "Heading1 are not equal");
 	   
 	   String heading2="Indian Institute of Technology Madras";
-	   WebElement text2=driver.findElement(By.xpath("(//h1[@class='head m-0']//following::h5)[1]"));
+	   WebElement text2=driver.findElement(By.xpath("(//h1[@class='head']//following::h5)[1]"));
 	   String Heading2 =text2.getText();
 	   System.out.println("Head2:"+Heading2);
 	   Assert.assertEquals(Heading2, heading2, "Heading2 are not equal");
